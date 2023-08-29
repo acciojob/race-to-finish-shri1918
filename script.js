168a7fd5ff7b2cb11927a4a07a2f3a1b5bef0ef0
Shridhar Savant <93203347+shri1918@users.noreply.github.com>
2023-08-29 09:19:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-window.promises = [];
+window.promises = [
 
+	new promise((resolve)=> setTimeOut(()=>resolve('promise 1') Math.random()*4000+1000)),
+	new promise((resolve)=> setTimeOut(()=> resolve('promise 2')Math.random ()*4000+1000)),
+	new promise((resolve)=> setTimeOut(()=> resolve('promise 3')Math.random ()*4000+1000)),
+	new promise((resolve)=> setTimeOut(()=> resolve('promise 4')Math.random ()*4000+1000)),
+	new promise((resolve)=> setTimeOut(()=> resolve('promise 5')Math.random ()*4000+1000)),
+	
+];
+Promise.any(promises)
+  .then((result) => {
+    // Printing the result to the output div
+    const outputDiv = document.getElementById('output');
+    outputDiv.textContent = result;
+  })
+  .catch((error) => {
+    console.error('All promises rejected:', error);
+  });
 // Do not change the code above this
 // add your promises to the array `promises`
